e378d3f73c03d660cbd18e242fbcfd7eba614bd7
dry-j <40339257+dry-j@users.noreply.github.com>
2020-02-08 21:46:52 +0100

    Update Auftragsakten.php

--- modules/Auftragsakten/Auftragsakten.php
+++ modules/Auftragsakten/Auftragsakten.php
@@ -1,2 +1,8 @@
 <?php
+include_once('vtlib/Vtiger/Module.php');
+
 class Auftragsakten {}
+
+
+$moduleInstance = Vtiger_Module::getInstance('ModuleName');
+$moduleInstance->addLink(<DETAILVIEWWIDGET>, <aUFTRAGSAKTEN>, <tESTAKTE>);
